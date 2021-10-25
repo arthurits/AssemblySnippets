cd005ed3d51190c3bff68df32ecfc86b7088bf93
—ins— <insht_cnct@hotmail.es>
2021-10-25 13:56:02 +0200

    Code updated

--- AssemblySnippets/Main.asm
+++ AssemblySnippets/Main.asm
@@ -14,7 +14,7 @@ else
 endif
 
 ; Constants and parameters
-EXIT_SUCCESS EQU <0>
+EXIT_SUCCESS EQU 0
 INVALID_HANDLE_VALUE equ -1;
 
 
@@ -45,17 +45,17 @@ externdef ReadConsoleW:PROC
 externdef WriteConsoleA:PROC
 externdef WriteConsoleW:PROC
   IFDEF __UNICODE__
-    WriteConsole equ ReadConsoleW
+    WriteConsole equ WriteConsoleW
   ELSE
-    WriteConsole equ ReadConsoleA
+    WriteConsole equ WriteConsoleA
   ENDIF
 
 ; void ExitProcess(
     ;   UINT uExitCode
     ; );
-extern ExitProcess:PROC
+externdef ExitProcess:PROC
 
-extern GetStdHandle : PROC
+externdef GetStdHandle : PROC
 
 .data
     hStdin QWORD 0
@@ -66,7 +66,7 @@ extern GetStdHandle : PROC
     ; Char buffers
     charsRead DWORD 0
     charsWritten DWORD 0
-    
+
     buffer BYTE 1024 DUP(0)
     BufferSize equ $-buffer
 
@@ -84,7 +84,7 @@ extern GetStdHandle : PROC
     msgErrorInputLength equ $-msgErrorInput
 
     msgErrorOutput BYTE "Error retrieving the output handle device", 0
-    msgErrorOutputLength equ $-msgErrorOutput
+    msgErrorOutputLength equ $-msgErrorOutput 
 .code
 
 main PROC
@@ -115,7 +115,8 @@ main PROC
 
     ; Read the user-input text
     mov rcx, hStdin
-    mov rdx, OFFSET buffer      ; adds a null value at the end of the buffer
+    lea rax, OFFSET buffer
+    mov rdx, rax      ; adds a null value at the end of the buffer
     mov r8d, BufferSize
     mov r9, OFFSET charsRead    ; includes the \n (13) and \r (10) chars but not the null value at the end
     call ReadConsoleA
@@ -123,47 +124,48 @@ main PROC
     ; Search any match with charTest
     mov ecx, charsRead
     ;inc rcx     ; uncomment this line in case we want to include in the loop the null character at the end of the string
-    LOOP1:
-    mov al, BYTE PTR [buffer + rcx - 1]
-    cmp al, charTest
-    je Yes
-    loop LOOP1
+    lea rax, OFFSET buffer
+    Loop1:        
+        mov al, BYTE PTR [rax + rcx - 1]
+        cmp al, charTest
+        je Yes
+    loop Loop1
 
     No:     ; No match found
-    mov rcx, hStdout
-    mov rdx, OFFSET msgNoMatch
-    mov r8d, msgNoMatchLength
-    mov r9, OFFSET charsWritten
-    call WriteConsoleA
-    jmp Exit
+        mov rcx, hStdout
+        mov rdx, OFFSET msgNoMatch
+        mov r8d, msgNoMatchLength
+        mov r9, OFFSET charsWritten
+        call WriteConsoleA
+        jmp Exit
 
     Yes:    ; Match found
-    mov rcx, hStdout
-    mov rdx, OFFSET msgMatch
-    mov r8d, msgMatchLength
-    mov r9, OFFSET charsWritten
-    call WriteConsoleA
-    jmp Exit
+        mov rcx, hStdout
+        mov rdx, OFFSET msgMatch
+        mov r8d, msgMatchLength
+        mov r9, OFFSET charsWritten
+        call WriteConsoleA
+        jmp Exit
 
     ; Show the error while getting the output handle
     ErrorHandleInput:
-    mov rdx, OFFSET msgErrorInput
-    mov r8d, msgErrorInputLength
-    jmp ErrorHandle
+        mov rdx, OFFSET msgErrorInput
+        mov r8d, msgErrorInputLength
+        jmp ErrorHandle
     
     ErrorHandleOutput:
-    mov rdx, Offset msgErrorOutput
-    mov r8d, msgErrorOutputLength
+        mov rdx, Offset msgErrorOutput
+        mov r8d, msgErrorOutputLength
 
     ErrorHandle:
-    mov rcx, hStdout
-    mov r9, OFFSET charsWritten
-    call WriteConsoleA
+        mov rcx, hStdout
+        mov r9, OFFSET charsWritten
+        call WriteConsoleA
 
     ; Exit the application
     Exit:
-    mov rcx, EXIT_SUCCESS
-    call ExitProcess
+        mov rcx, EXIT_SUCCESS
+        call ExitProcess
 
 main ENDP
 
